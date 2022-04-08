e9c58570247a86247abc8a998780ccdb2579878a
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-04-08 20:00:13 -0300

    Adicionandos componentes e libs  ao projeto

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color: '#44343', expense: true },
+    rent: { title: 'Aluguel', color: 'brown', expense: true },
+    salary: { title: 'Sálario', color: 'gren', expense: false }
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] =  [
+    { date: new Date(2022, 3, 08), category: 'food', title: 'McDonalds', value: 32.15 },
+    { date: new Date(2022, 3, 08), category: 'food', title: 'Burger King', value: 32.15 },
+    { date: new Date(2022, 3, 10), category: 'rent', title: 'Aluguel ', value: 750 },
+    { date: new Date(2022, 4, 18), category: 'salary', title: 'Sálario ', value: 1.300 },
+];
